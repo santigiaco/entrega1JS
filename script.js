6d71f270070a1d44eaa3f8b9069f4f01949b2512
santiago giacometti <santiagogiaco@gmail.com>
2022-06-30 19:59:23 -0300

    primera carga

---
+++ script.js
@@ -0,0 +1,32 @@
+// Ejemplo 1
+// let texto1 = "Hola,"
+// let nombre = "Santiago"
+// let saludo = texto1 + nombre
+// console.log (saludo)
+
+// Ejemplo 2
+// let precio = prompt("ingrese un numero");
+// if (precio < 20) {
+//     console.log("el precio es menor a 20");
+// } else if (precio > 50) {
+//     console.log("el precio es mayor a 50");
+// } else if (precio > 35) {
+//     console.log("el precio es mayor a 35");
+//
+
+// Ejemplo 3
+// let nombre = prompt("ingrese un nombre");
+// switch (nombre) {
+//     case "ana":
+//         console.log("el dni de juan es : 3362654");
+//         break;
+//     case "marcos":
+//         console.log("el dni de mariela es : 3362544");
+//         break;
+//     case "ariel":
+//         console.log("el dni de Juana es : 3782654");
+//         break;
+//     default:
+//         console.log("no existe esa informacion");
+//         break;
+// }
